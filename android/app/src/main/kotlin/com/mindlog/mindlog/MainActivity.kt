4b667e3548afa2ba638346a42b37aad9d879bd3d
Chan Tai Yu <taiyuchan04@gmail.com>
2026-06-17 21:20:34 +0800

    Scaffold MindLog Flutter project with deps and Android config

---
+++ android/app/src/main/kotlin/com/mindlog/mindlog/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mindlog.mindlog
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+// FlutterFragmentActivity is required by local_auth for the biometric prompt.
+class MainActivity : FlutterFragmentActivity()
